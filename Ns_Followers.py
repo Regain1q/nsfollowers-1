5120c7a75520eadfb504f87f8215ea5dd7650ee2
Nesterexx <rohitkunwar1236@gmail.com>
2024-05-17 12:30:27 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='66439659683' #Enter UserID Here
-        self.__username='nazanin._najafi6168' #Enter UserName Here
-        self._session='66439659683:nqKUIn1awkZFTj:18:AYcftgasDHFRZPOqIoTfrxA50w6LDujprtuSfGld2g' #Enter SessionID Here
+        self.__userid='66766931903' #Enter UserID Here
+        self.__username='ataei.kamran_4998' #Enter UserName Here
+        self._session='66766931903:eW4E7gS7abugO1:27:AYdHmi780U8Rjw5iD4G9PpvA0qIQFMrkwKYkhRSMIw' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
@@ -116,10 +116,10 @@ if __name__ == '__main__':
             if "suggests" in suff.text:
                 for items in suff.json()['suggests']:
                     print(cl.PlaceOrder(items))
-                    time.sleep(random.randint(10,15))
+                    time.sleep(random.randint(0,0))
             else:
                 print(suff.text)
-                time.sleep(10)
+                time.sleep(0)
         except Exception as E:
             print(E)
-            time.sleep(5)
+            time.sleep(0)
